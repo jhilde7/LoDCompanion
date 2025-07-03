daa2cc49b6fe41a56a14b5023eb0ea4e0b4e8ee8
Jacob Hildebrand <j.hildebrandx@gmail.com>
2025-07-03 15:18:18 -0600

    Quest class and associated enums for the quest templates

---
+++ Models/Dungeon/Quest.cs
@@ -0,0 +1,56 @@
+﻿using LoDCompanion.Services.Dungeon;
+
+namespace LoDCompanion.Models.Dungeon
+{
+    public enum QuestLocation
+    {
+        Caelkirk,
+        Coalfell,
+        Freyfall,
+        Irondale,
+        Rochdale,
+        SilverCity,
+        TheOutpost,
+        Windfair,
+        Whiteport
+    }
+
+    public enum EncounterType
+    {
+         Beasts,
+         Undead,
+         Bandits,
+         Orcs,
+         Reptiles,
+         DarkElves,
+         AncientLands,
+         GoblinKing,
+         SpringCleaning,
+         C26,
+         C29,
+         R17,
+         R19,
+         R20,
+         R28,
+         R30,
+         TombGuardian
+    }
+
+    public class Quest
+    {
+        public QuestLocation Location { get; private set;}
+        public string SpecialRules { get; private set;} = string.Empty;
+        public int CorridorCount { get; private set;}
+        public int RoomCount { get; private set;}
+        public int RewardCoin { get; private set;}
+        public string RewardSpecial { get; private set;} = string.Empty;
+        public EncounterType EncounterType { get; private set;}
+        public RoomInfo? ObjectiveRoom { get; private set;}
+        public int StartThreatLevel { get; private set;}
+        public int MinThreatLevel { get; private set;}
+        public int MaxThreatLevel { get; private set;}
+        public string NarrativeObjectiveRoom { get; private set; } = string.Empty;
+        public string NarrativeSetup { get; private set;} = string.Empty;
+        public string NarrativeAftermath { get; private set; } = string.Empty;
+    }
+}
