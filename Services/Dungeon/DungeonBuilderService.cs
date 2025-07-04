8040bb4a744e6c1e70e40628c3436c4863f2ae53
Jacob Hildebrand <j.hildebrandx@gmail.com>
2025-07-04 14:37:50 -0600

    Refined DungeonBuilder logic to be more accurate. Removed Settlement as it is unused at this time

--- Services/Dungeon/DungeonBuilderService.cs
+++ Services/Dungeon/DungeonBuilderService.cs
@@ -21,21 +21,30 @@ namespace LoDCompanion.Services.Dungeon
             var rooms = BuildRoomList(roomCount, roomsToExclude);
             var corridors = BuildCorridorList(corridorCount, corridorsToExclude);
 
-            deck.AddRange(rooms);
-            deck.AddRange(corridors);
+            var initialDeck = new List<RoomInfo>();
+            initialDeck.AddRange(rooms);
+            initialDeck.AddRange(corridors);
+            initialDeck.Shuffle();
 
-            // 2. Shuffle the deck
-            deck.Shuffle();
+            // 2. Divide the deck into two equal (or near-equal) piles.
+            var halfDeckSize = initialDeck.Count / 2;
+            var firstHalf = initialDeck.Take(halfDeckSize).ToList();
+            var secondHalf = initialDeck.Skip(halfDeckSize).ToList();
 
-            // 3. Place the objective room in the second half of the deck
-            var objectiveRoomInfo = _gameData.RoomInfo.FirstOrDefault(r => r.Name == objectiveRoom);
+            // 3. Add the objective room to one of the piles and shuffle that pile.
+            var objectiveRoomInfo = _gameData.RoomInfo.First(r => r.Name == objectiveRoom);
             if (objectiveRoomInfo != null)
             {
-                var halfDeckSize = deck.Count / 2;
-                deck.Insert(RandomHelper.GetRandomNumber(halfDeckSize, deck.Count), objectiveRoomInfo);
+                secondHalf.Add(objectiveRoomInfo);
+                secondHalf.Shuffle();
             }
 
-            return deck;
+            // 4. Combine the piles, placing the pile with the objective at the bottom. 
+            var finalDeck = new List<RoomInfo>();
+            finalDeck.AddRange(firstHalf);
+            finalDeck.AddRange(secondHalf);
+
+            return finalDeck;
         }
 
         private List<RoomInfo> BuildRoomList(int count, List<string> excludedRooms)
