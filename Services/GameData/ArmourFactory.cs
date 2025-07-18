abc3973f5bccd8805c53c18d642d60f784538350
Jacob Hildebrand <j.hildebrandx@gmail.com>
2025-07-18 07:32:08 -0600

    Added factory with armour and shield modifier methods

---
+++ Services/GameData/ArmourFactory.cs
@@ -0,0 +1,39 @@
+﻿using LoDCompanion.Models;
+
+namespace LoDCompanion.Services.GameData
+{
+    public class ArmourFactory
+    {
+        public Armour CreateModifiedArmour(
+            string baseArmourName,
+            string newName,
+            Action<Armour> modifications)
+        {
+            // 1. Find the base template.
+            Armour template = EquipmentService.GetArmourByName(baseArmourName) ?? new Armour();
+
+            Armour newArmour = new Armour(template);
+            newArmour.Name = newName;
+
+            modifications(newArmour);
+
+            return newArmour;
+        }
+
+        public Shield CreateModifiedShield(
+            string baseShieldName,
+            string newName,
+            Action<Shield> modifications)
+        {
+            // 1. Find the base template.
+            Shield template = EquipmentService.GetShieldByName(baseShieldName) ?? new Shield();
+
+            Shield newShield = new Shield(template);
+            newShield.Name = newName;
+
+            modifications(newShield);
+
+            return newShield;
+        }
+    }
+}
