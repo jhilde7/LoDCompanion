f9941b59a39bcc7a65005c188135aefd78953452
Jacob Hildebrand <j.hildebrandx@gmail.com>
2025-07-21 11:34:19 -0600

    Updated to remove 0 quantity items form backpack

--- Utilities/BackpackHelper.cs
+++ Utilities/BackpackHelper.cs
@@ -38,10 +38,6 @@ namespace LoDCompanion.Utilities
         internal static Equipment? TakeOneItem(List<Equipment> backpack, Equipment item)
         {
             var itemInBackPack = backpack.FirstOrDefault(i => i.Name == item.Name);
-            if (itemInBackPack != null && itemInBackPack.Quantity == 1)
-            {
-                return itemInBackPack;
-            }
 
             if (itemInBackPack != null)
             {
@@ -49,42 +45,70 @@ namespace LoDCompanion.Utilities
                 {
                     var meleeWeapon = new MeleeWeapon(melee);
                     melee.Quantity -= 1;
+                    if (melee.Quantity <= 0)
+                    {
+                        backpack.Remove(meleeWeapon);
+                    }
                     return meleeWeapon;
                 }
                 else if (itemInBackPack is RangedWeapon ranged)
                 {
                     var rangedWeapon = new RangedWeapon(ranged);
                     ranged.Quantity -= 1;
+                    if (ranged.Quantity <= 0)
+                    {
+                        backpack.Remove(rangedWeapon);
+                    }
                     return rangedWeapon;
                 }
                 else if (itemInBackPack is Armour armour)
                 {
                     var armourItem = new Armour(armour);
                     armour.Quantity -= 1;
+                    if (armour.Quantity <= 0)
+                    {
+                        backpack.Remove(armourItem);
+                    }
                     return armourItem;
                 }
                 else if (itemInBackPack is Shield shield)
                 {
                     var shieldItem = new Shield(shield);
                     shield.Quantity -= 1;
+                    if (shield.Quantity <= 0)
+                    {
+                        backpack.Remove(shieldItem);
+                    }
                     return shieldItem;
                 }
                 else if (itemInBackPack is Ammo ammo)
                 {
                     var ammoItem = new Ammo(ammo);
                     ammo.Quantity -= 1;
+                    if (ammo.Quantity <= 0)
+                    {
+                        backpack.Remove(ammoItem);
+                    }
                     return ammoItem;
                 }
                 else if (itemInBackPack is MagicStaff mStaff)
                 {
                     var staff = new MagicStaff(mStaff);
                     mStaff.Quantity -= 1;
+                    if (mStaff.Quantity <= 0)
+                    {
+                        backpack.Remove(staff);
+                    }
                     return staff;
                 }
                 else
                 {
                     var singleItem = new Equipment(itemInBackPack);
                     itemInBackPack.Quantity -= 1;
+                    if (itemInBackPack.Quantity <= 0)
+                    {
+                        backpack.Remove(singleItem);
+                    }
                     return singleItem;
                 }
             }
