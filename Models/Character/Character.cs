ac25d0f95ddf476d52b71a388672037e43fccf8f
Jacob Hildebrand <j.hildebrandx@gmail.com>
2025-07-25 07:14:00 -0600

    added tracking properties for levelup

--- Models/Character/Character.cs
+++ Models/Character/Character.cs
@@ -324,6 +324,10 @@ namespace LoDCompanion.Models.Character
         public bool HasBeenTargetedThisTurn { get; set; }
         public bool HasDodgedThisBattle { get; set; } = false;
 
+        public int ImprovementPoints { get; set; } // Points used for improving stats or skills
+        public Dictionary<BasicStat, int> BasicStatsImprovedThisLevel { get; set; } = new Dictionary<BasicStat, int>();
+        public Dictionary<Skill, int> SkillsImprovedThisLevel { get; set; } = new Dictionary<Skill, int>();
+
         // Constructor
         public Hero() : base() { }
 
