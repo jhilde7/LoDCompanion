9e0ec3a8adf06807aa32eb4f9345e2be72cea6ed
Jacob Hildebrand <j.hildebrandx@gmail.com>
2025-08-19 09:00:25 -0600

    Updated threat service to include complete processing of threat level roll and results.

--- BackEnd/Services/Dungeon/ThreatService.cs
+++ BackEnd/Services/Dungeon/ThreatService.cs
@@ -1,8 +1,9 @@
 ﻿using LoDCompanion.BackEnd.Services.GameData;
 using LoDCompanion.BackEnd.Services.Player;
 using LoDCompanion.BackEnd.Services.Utilities;
-using System;
+using LoDCompanion.BackEnd.Models;
 using System.Threading.Tasks;
+using LoDCompanion.BackEnd.Services.Combat;
 
 namespace LoDCompanion.BackEnd.Services.Dungeon
 {
@@ -15,7 +16,39 @@ namespace LoDCompanion.BackEnd.Services.Dungeon
         public bool SpawnWanderingMonster { get; set; } = false;
         public bool SpawnTrap { get; set; } = false;
         public bool ShouldAddExplorationCards { get; internal set; }
-        // Add other properties here for more complex events, e.g., reinforcements
+        public Room? SpawnRandomEncounter { get; internal set; }
+        public bool ThreatEventTriggered => SpawnWanderingMonster || SpawnTrap || ShouldAddExplorationCards || SpawnRandomEncounter != null;
+    }
+
+    public enum ThreatActionType
+    {
+        // Increases
+        WinBattle,
+        OpenDoorOrChest,
+        ClearCobweb,
+        ThreatRollExceeded,
+        BashLock,
+        BashLockWithCrowbar,
+
+        // Decreases from Threat Events
+        WanderingMonsterSpawned,
+        ExtraExplorationCards,
+        EncounterRiskIncreased,
+        TrapSprung,
+        ScenarioDieModified,
+        Rest,
+        PerfectRoll,
+
+        // Decreases from In-Battle Events
+        DisturbanceInTheVoid,
+        GreenishTint,
+        ForgedUnderPressure,
+        EnemyHealing,
+        EnemyFrenzy,
+        HeroDisarmed,
+        EnemyBecomesFearsome,
+        Reinforcements,
+        Onwards,
     }
 
     /// <summary>
@@ -23,15 +56,18 @@ namespace LoDCompanion.BackEnd.Services.Dungeon
     /// </summary>
     public class ThreatService
     {
-        private readonly UserRequestService _userRequest;
         private readonly PowerActivationService _powerActivation;
         private readonly DungeonState _dungeon;
+        private readonly EncounterService _encounter;
 
-        public ThreatService(UserRequestService userRequestService, PowerActivationService powerActivationService, DungeonState dungeon)
+        public ThreatService(
+            PowerActivationService powerActivationService, 
+            DungeonState dungeon,
+            EncounterService encounterService)
         {
-            _userRequest = userRequestService;
             _powerActivation = powerActivationService;
             _dungeon = dungeon;
+            _encounter = encounterService;
         }
 
         /// <summary>
@@ -41,6 +77,11 @@ namespace LoDCompanion.BackEnd.Services.Dungeon
         /// <param name="amount">The amount to increase the threat by.</param>
         public void IncreaseThreat(int amount)
         {
+            if(amount < 0)
+            {
+                amount = Math.Abs(amount);
+                DecreaseThreat(amount);
+            }
             _dungeon.ThreatLevel += amount;
             Console.WriteLine($"Threat increased by {amount}. New Threat Level: {_dungeon.ThreatLevel}");
         }
@@ -90,14 +131,14 @@ namespace LoDCompanion.BackEnd.Services.Dungeon
 
             if (threatRoll == 20)
             {
-                DecreaseThreat(5);
+                UpdateThreatLevelByThreatActionType(ThreatActionType.PerfectRoll);
                 return new ThreatEventResult { Description = "A moment of calm. Threat decreases by 5." };
             }
 
             if (threatRoll <= _dungeon.ThreatLevel)
             {
                 // A threat event is triggered!
-                return ResolveThreatEvent(isInBattle);
+                return await ResolveThreatEventAsync(isInBattle);
             }
 
             return new ThreatEventResult { Description = "Nothing happens" };
@@ -106,12 +147,12 @@ namespace LoDCompanion.BackEnd.Services.Dungeon
         /// <summary>
         /// Resolves a triggered threat event based on whether the party is in combat.
         /// </summary>
-        private ThreatEventResult ResolveThreatEvent(bool isInBattle)
+        private async Task<ThreatEventResult> ResolveThreatEventAsync(bool isInBattle)
         {
             ThreatEventResult result;
             if (isInBattle)
             {
-                result = ResolveInBattleEvent();
+                result = await ResolveInBattleEvent();
             }
             else
             {
@@ -131,29 +172,29 @@ namespace LoDCompanion.BackEnd.Services.Dungeon
 
             switch (roll)
             {
-                case int n when n >= 1 && n <= 12:
+                case <= 12:
                     result.Description = "A Wandering Monster has appeared!";
-                    DecreaseThreat(5);
+                    UpdateThreatLevelByThreatActionType(ThreatActionType.WanderingMonsterSpawned);
                     result.SpawnWanderingMonster = true;
                     break;
-                case int n when n >= 13 && n <= 15:
+                case <= 15:
                     result.Description = "The dungeon shifts... Add one extra Exploration Card on top of each pile.";
-                    DecreaseThreat(5);
+                    UpdateThreatLevelByThreatActionType(ThreatActionType.ExtraExplorationCards);
                     result.ShouldAddExplorationCards = true;
                     break;
-                case int n when n >= 16 && n <= 17:
+                case <= 17:
                     result.Description = "The air grows heavy. The risk of encounters has gone up by 10% for the rest of the quest.";
-                    DecreaseThreat(6);
+                    UpdateThreatLevelByThreatActionType(ThreatActionType.EncounterRiskIncreased);
                     _dungeon.EncounterChanceModifier += 10;
                     break;
-                case int n when n >= 18 && n <= 19:
+                case <= 19:
                     result.Description = "A hero has sprung a trap!";
-                    DecreaseThreat(7);
+                    UpdateThreatLevelByThreatActionType(ThreatActionType.TrapSprung);
                     result.SpawnTrap = true;
                     break;
                 case 20:
                     result.Description = "A strange energy fills the dungeon. Add +1 on all Scenario die rolls for the remainder of the dungeon.";
-                    DecreaseThreat(10);
+                    UpdateThreatLevelByThreatActionType(ThreatActionType.ScenarioDieModified);
                     _dungeon.ScenarioRollModifier += 1;
                     break;
             }
@@ -163,52 +204,142 @@ namespace LoDCompanion.BackEnd.Services.Dungeon
         /// <summary>
         /// Handles the "If the party is in battle" event table from the rulebook.
         /// </summary>
-        private ThreatEventResult ResolveInBattleEvent()
+        private async Task<ThreatEventResult> ResolveInBattleEvent()
         {
             int roll = RandomHelper.RollDie(DiceType.D10);
             var result = new ThreatEventResult();
+            var monsters = _dungeon.RevealedMonsters.ToList();
+            var heroes = _dungeon.HeroParty?.Heroes.ToList();
 
             switch (roll)
             {
                 case 1:
                     result.Description = "A disturbance in the Void! Spell Casters may do nothing during the coming turn.";
-                    DecreaseThreat(2);
+                    var spellCasters = _dungeon.HeroParty?.Heroes.Where(h => h.CanCastSpell = true);
+                    foreach (var caster in spellCasters ?? Enumerable.Empty<Hero>())
+                    {
+                        await StatusEffectService.AttemptToApplyStatusAsync(caster, 
+                            new ActiveStatusEffect(StatusEffectType.Incapacitated, 1), _powerActivation);
+                    }
+                    UpdateThreatLevelByThreatActionType(ThreatActionType.DisturbanceInTheVoid);
                     break;
                 case 2:
-                    result.Description = "A greenish tint appears on the enemies' weapons! They gain the Poisonous Special Rule.";
-                    DecreaseThreat(2);
+                    monsters.Shuffle();
+                    var monsterToModify = monsters.FirstOrDefault(m => m.ActiveWeapon != null);
+                    monsterToModify?.ActiveWeapon?.Properties.Add(WeaponProperty.Poisoned, 1);
+                    result.Description = $"A greenish tint appears on an enemies' weapons! {monsterToModify?.Name} gains the Poisonous.";
+                    UpdateThreatLevelByThreatActionType(ThreatActionType.GreenishTint);
                     break;
                 case 3:
-                    result.Description = "Forged under pressure! One enemy gains +15 CS until dead.";
-                    DecreaseThreat(3);
+                    monsters.Shuffle();
+                    await StatusEffectService.AttemptToApplyStatusAsync(monsters[0], 
+                        new ActiveStatusEffect(StatusEffectType.ForgedUnderPressure, -1, skillBonus: (Skill.CombatSkill, 15)), _powerActivation);
+                    result.Description = $"Forged under pressure! {monsters[0].Name} gains +15 CS until dead.";
+                    UpdateThreatLevelByThreatActionType(ThreatActionType.ForgedUnderPressure);
                     break;
                 case int n when n >= 4 && n <= 5:
-                    result.Description = "Divine intervention? One wounded enemy heals 1d10 Hit Points.";
-                    DecreaseThreat(3);
+                    var WoundedMonsters = monsters.Where(m => m.CurrentHP < m.GetStat(BasicStat.HitPoints)).OrderBy(m => m.CurrentHP < m.GetStat(BasicStat.HitPoints));
+                    int amountToHeal = RandomHelper.RollDie(DiceType.D10);
+                    if (WoundedMonsters.Any()) WoundedMonsters.First().Heal(amountToHeal);
+                    result.Description = $"Divine intervention? {WoundedMonsters.First()} heals {amountToHeal} Hit Points.";
+                    UpdateThreatLevelByThreatActionType(ThreatActionType.EnemyHealing);
                     break;
                 case 6:
-                    result.Description = "Frenzy! One enemy gains the Frenzy Special Rule until dead.";
-                    DecreaseThreat(3);
+                    monsters.Shuffle();
+                    await StatusEffectService.AttemptToApplyStatusAsync(monsters[0], 
+                        new ActiveStatusEffect(StatusEffectType.Frenzy, -1), _powerActivation);
+                    result.Description = $"Frenzy! {monsters[0].Name} gains Frenzy until dead.";
+                    UpdateThreatLevelByThreatActionType(ThreatActionType.EnemyFrenzy);
                     break;
                 case 7:
-                    result.Description = "Disarmed! A random hero drops their weapon and must spend an action to retrieve it.";
-                    DecreaseThreat(3);
+                    heroes?.Shuffle();
+                    var disarmedHero = heroes?[0];
+                    if (disarmedHero != null && disarmedHero.Inventory.EquippedWeapon != null)
+                    {
+                        disarmedHero.DroppedWeapon = disarmedHero.Inventory.EquippedWeapon;
+                        disarmedHero.Inventory.EquippedWeapon = null;
+                    }
+                    result.Description = $"Disarmed! A {disarmedHero?.Name} drops their weapon and must spend an action to retrieve it.";
+                    UpdateThreatLevelByThreatActionType(ThreatActionType.HeroDisarmed);
                     break;
                 case 8:
-                    result.Description = "Fearsome! One enemy gains the Fear Special Rule.";
-                    DecreaseThreat(4);
+                    monsters.Shuffle();
+                    monsters[0].PassiveSpecials.Add(Game.MonsterSpecialName.CauseFear, 10);
+                    result.Description = $"Fearsome! {monsters[0].Name} gains the Fear passive.";
+                    UpdateThreatLevelByThreatActionType(ThreatActionType.EnemyBecomesFearsome);
                     break;
                 case 9:
+                    var doors = _dungeon.CurrentRoom?.Doors;
+                    doors?.Shuffle();
+                    var roomForEncounter = doors?[0].ConnectedRooms?.FirstOrDefault(r => r != _dungeon.CurrentRoom);
+                    result.SpawnRandomEncounter = roomForEncounter;
                     result.Description = "Reinforcements! A new encounter appears at a random door.";
-                    DecreaseThreat(4);
-                    // Note: This would trigger logic in DungeonManagerService to spawn more monsters.
+                    UpdateThreatLevelByThreatActionType(ThreatActionType.Reinforcements);
                     break;
                 case 10:
                     result.Description = "Onwards! All enemies gain +10 CS until the end of the battle.";
-                    DecreaseThreat(6);
+                    foreach (var monster in monsters)
+                    {
+                        await StatusEffectService.AttemptToApplyStatusAsync(monster, 
+                            new ActiveStatusEffect(StatusEffectType.ForgedUnderPressure, -1, skillBonus: (Skill.CombatSkill, 10)), _powerActivation);
+                    }
+                    UpdateThreatLevelByThreatActionType(ThreatActionType.Onwards);
                     break;
             }
             return result;
+        }
+
+        public void UpdateThreatLevelByThreatActionType (ThreatActionType threatAction)
+        {
+            int changeAmount = 0;
+
+            switch (threatAction)
+            {
+                case ThreatActionType.WinBattle:
+                case ThreatActionType.OpenDoorOrChest:
+                case ThreatActionType.ClearCobweb:
+                case ThreatActionType.ThreatRollExceeded:
+                case ThreatActionType.BashLockWithCrowbar:
+                    changeAmount = 1;
+                    break;
+                case ThreatActionType.BashLock:
+                    changeAmount = 2;
+                    break;
+                case ThreatActionType.ExtraExplorationCards:
+                case ThreatActionType.WanderingMonsterSpawned:
+                case ThreatActionType.Rest:
+                case ThreatActionType.PerfectRoll:
+                    changeAmount = -5;
+                    break;
+                case ThreatActionType.EncounterRiskIncreased:
+                    changeAmount = -6;
+                    break;
+                case ThreatActionType.TrapSprung:
+                    changeAmount = -7;
+                    break;
+                case ThreatActionType.ScenarioDieModified:
+                    changeAmount = -10;
+                    break;
+                case ThreatActionType.DisturbanceInTheVoid:
+                case ThreatActionType.GreenishTint:
+                    changeAmount = -2;
+                    break;
+                case ThreatActionType.ForgedUnderPressure:
+                case ThreatActionType.EnemyHealing:
+                case ThreatActionType.EnemyFrenzy:
+                case ThreatActionType.HeroDisarmed:
+                    changeAmount = -3;
+                    break;
+                case ThreatActionType.EnemyBecomesFearsome:
+                case ThreatActionType.Reinforcements:
+                    changeAmount = -4;
+                    break;
+                case ThreatActionType.Onwards:
+                    changeAmount = -6;
+                    break;
+            }
+
+            IncreaseThreat(changeAmount);
         }
     }
 }
